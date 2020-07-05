b647065d3b978941619b2f572d26267d182b5833
Paula <paulsa07@ucm.es>
2020-07-05 19:59:09 +0200

    ejercicio para paracticar variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+# Escribe un programa que dado el valor del radio de una circunferencia,
+# imprima el valor de su longitud y el área del círculo que delimita.
+# Para hacerlo sigue los siguiente pasos:
+
+# Declara una variable pi y asígnale el valor 3.14159.
+numeroPi = 3.14159
+
+# Declara una variable radio y asígnale el valor 5.
+radio = 5
+
+# Usa la siguientes fórmulas para calcular la longitud de la circunferencia y el área del círculo:
+longitud = 2 * numeroPi * radio
+area = numeroPi * radio * radio
+
+# Imprime el resultado
+print("longitud del circulo: ", longitud , ", y el area es: " ,area)
